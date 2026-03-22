74c6caabd41faf86bf6610112154d65750f971c2
Inochi <sunyifu2022@outlook.com>
2026-03-22 16:43:05 +0800

    完善了大部分逻辑，添加了粒子效果，有盾时障碍物会虚线描边，新增了游戏结束时的提示。减少bug。小概率情况当帧数正好是60倍数且游戏状态变为PAUSE时console会刷屏

--- c++miniGame/miniGame.cpp
+++ c++miniGame/miniGame.cpp
@@ -21,9 +21,13 @@ bool bgmState = false; // 音乐开启状态
 bool wallActive = true; // 隔离墙是否激活
 float totalScore = 0; // 计分
 float currentDifficulty = 1.0f; // 难度系数
+// 道具增益效果
+bool yangHasShield = false; // 阳星是否有盾
+bool yinHasShield = false;  // 阴星是否有盾
+int slowDownTimer = 0; // 减速效果计时器
 
 // 双星	
- struct Twin {
+struct Twin {
 	float x, y; // 位置
 	int radius; // 半径
 	COLORREF color;
@@ -32,43 +36,54 @@ float currentDifficulty = 1.0f; // 难度系数
 	float trail_y[10];   // 过去 10 帧的 Y 坐标
 };
 
- // 背景星空
+// 背景星空
 #define STAR_COUNT 50
- struct Star {
-	 float x, y;
-	 float speed;
-	 int brightness; // 亮度
- }stars[STAR_COUNT];
+struct Star {
+	float x, y;
+	float speed;
+	int brightness; // 亮度
+}stars[STAR_COUNT];
 
- // 道具
- enum PowerType {
-	 WALL_BREAKER,SHIELD,SLOW_DOWN // 消墙、盾、减速
- };
- struct PowerUp {
-	 float x, y;
-	 int radius;
-	 bool active;
-	 //PowerType type;
-	 COLORREF color;
- } wallBreaker = { 0, 0, 12, false, RGB(255, 215, 0) }; // 金黄色，显眼
+// 道具
+enum PowerType {
+	WALL_BREAKER, SHIELD, SLOW_DOWN // 消墙、盾、减速
+};
+struct PowerUp {
+	float x, y;
+	int radius;
+	bool active;
+	PowerType type;
+	COLORREF color;
+};
+#define MAX_POWERS 3 // 同屏最大道具数量
+struct PowerUp powers[MAX_POWERS] = { 0 };
 
-
- // 障碍物
- struct Obstacle {
-	 float x, y;
-	 float w, h;      // 宽和高
-	 bool active;     // 是否在屏幕上（存活状态）
-	 COLORREF color;  // 障碍物颜色
- };
+// 障碍物
+struct Obstacle {
+	float x, y;
+	float w, h;      // 宽和高
+	bool active;     // 是否在屏幕上（存活状态）
+	COLORREF color;  // 障碍物颜色
+};
 #define MAX_OBS 32   // 同屏最多 8 个障碍物
- struct Obstacle obs[MAX_OBS] = { 0 };
- int spawnTimer = 0; // 生成计时器
+struct Obstacle obs[MAX_OBS] = { 0 };
+int spawnTimer = 0; // 生成计时器
 
- // 游戏状态
- enum State {
-	 MENU, PLAYING,PAUSE, GAMEOVER
- };
+// 游戏状态
+enum State {
+	MENU, PLAYING, PAUSE, GAMEOVER
+};
 
+// 粒子效果
+struct Particle {
+	float x, y;
+	float vx, vy; // 速度
+	int life;     // 生命周期
+	COLORREF color;
+	bool active;
+};
+#define MAX_PARTICLES 100
+struct Particle particles[MAX_PARTICLES] = { 0 };
 
 // 创建按钮
 void CreatButton(POINT pos, int width, int height, LPCTSTR text, COLORREF col) {
@@ -279,7 +294,7 @@ void DrawTrails(struct Twin* star, COLORREF col) {
 	}
 }
 
-// 判断小球是否与障碍物重叠
+// 判断小球是否与障碍物重叠，后面修改一下可以适用道具碰撞检测
 bool CheckCollision(struct Twin* star, struct Obstacle* ob) {
 	if (!ob->active) return false;
 	// 碰撞判定
@@ -294,12 +309,16 @@ bool CheckCollision(struct Twin* star, struct Obstacle* ob) {
 
 // 生成与更新障碍物
 void UpdateObstacles() {
-	float obsSpeed = 6.0f * currentDifficulty; // 障碍物飞过来的速度
+	float baseSpeed = 6.0f * currentDifficulty; // 基础速度
+	float obsSpeed = (slowDownTimer > 0) ? (baseSpeed * 0.5f) : baseSpeed;
+	if (slowDownTimer > 0)
+		slowDownTimer--;
+
 	// 每隔 45 帧生成一个新障碍物，大约0.75秒
 	spawnTimer++;
 	if (spawnTimer >= 45) {
 		spawnTimer = 0;
-		// 在数组里找“非激活”状态的空位生成新障碍物
+		// 找非激活状态空位生成
 		for (int i = 0; i < MAX_OBS; i++) {
 			if (!obs[i].active) {
 				obs[i].active = true;
@@ -319,7 +338,7 @@ void UpdateObstacles() {
 	for (int i = 0; i < MAX_OBS; i++) {
 		if (obs[i].active) {
 			obs[i].x -= obsSpeed; // 向左移动
-			// 如果移出了屏幕左侧，回收它
+			// 超出屏幕左侧回收
 			if (obs[i].x + obs[i].w < 0) {
 				obs[i].active = false;
 			}
@@ -327,8 +346,126 @@ void UpdateObstacles() {
 	}
 }
 
+
+
+// 粒子特效
+void SpawnExplosion(float x, float y, COLORREF color) {
+	int count = 15 + rand() % 10; // 每次爆出 15~25 个粒子
+	for (int i = 0; i < MAX_PARTICLES && count > 0; i++) {
+		if (!particles[i].active) {
+			particles[i].active = true;
+			particles[i].x = x;
+			particles[i].y = y;
+			// 随机生成向四周散射的速度
+			float angle = (rand() % 360) * 3.14159f / 180.0f;
+			float speed = (float)(rand() % 50 + 20) / 10.0f; // 2.0 ~ 7.0 的速度
+			particles[i].vx = cos(angle) * speed;
+			particles[i].vy = sin(angle) * speed;
+			particles[i].life = 20 + rand() % 20; // 存活 20~40 帧
+			particles[i].color = color;
+			count--;
+		}
+	}
+}
+
+// 更新并绘制粒子
+void UpdateAndDrawParticles() {
+	for (int i = 0; i < MAX_PARTICLES; i++) {
+		if (particles[i].active) {
+			particles[i].x += particles[i].vx;
+			particles[i].y += particles[i].vy;
+			particles[i].life--;
+
+			if (particles[i].life <= 0) {
+				particles[i].active = false; // 回收
+			}
+			else {
+				// 粒子随着寿命减少，半径变小
+				int r = particles[i].life / 10 + 1;
+				setfillcolor(particles[i].color);
+				solidcircle((int)particles[i].x, (int)particles[i].y, r);
+			}
+		}
+	}
+}
+
+// 更新道具状态
+void UpdatePowers(struct Twin* yang, struct Twin* yin, clock_t totalFrames) {
+	// 生成逻辑
+	if (totalFrames % 300 == 0) {
+		for (int i = 0; i < MAX_POWERS; i++) {
+			if (!powers[i].active) {
+				powers[i].active = true;
+				powers[i].x = 800;
+				powers[i].y = 100 + rand() % 400; // 随机Y轴
+				powers[i].radius = 12;
+				// 随记道具类型
+				// 可能大概有BUG，比如重复道具但是检测到不满足条件不会生成？
+				int type = 2; // 默认减速
+				if(wallActive == 1)
+					type = rand() % 3; // 有墙时三种道具都有可能
+				if(wallActive == 0)
+					type = 1 + rand() % 2; // 没墙了就不生成破墙道具
+
+				powers[i].type = (PowerType)type;
+				if (type == 0) {
+					powers[i].color = RGB(255, 215, 0); // 破！
+				}
+				else if (type == 1 && !yangHasShield && !yinHasShield) {
+					powers[i].color = RGB(0, 255, 150); // 安如磐石
+				}
+				else {
+					powers[i].color = RGB(200, 50, 255); // 紫色心情减速
+				}
+				printf("[LOG]:生成道具 %d 类型 %d\n", i, type);
+				break;
+			}
+		}
+	}
+	// 绘制道具和碰撞检测以及触发
+	for (int i = 0; i < MAX_POWERS; i++) {
+		if (powers[i].active) {
+			// 移动逻辑
+			powers[i].x -= 4.0f;
+			// 闪烁效果
+			int breath = (totalFrames % 30 == 0) ? 2 : 0;
+			setfillcolor(powers[i].color);
+			solidcircle((int)powers[i].x, (int)powers[i].y, powers[i].radius + breath);
+			// 碰撞检测
+			bool caughtByYang = (abs(yang->x - powers[i].x) < 20 && abs(yang->y - powers[i].y) < 20);
+			bool caughtByYin = (abs(yin->x - powers[i].x) < 20 && abs(yin->y - powers[i].y) < 20);
+			if (caughtByYang || caughtByYin) {
+				printf("[LOG]:道具 %d 被 %s 捕获\n", i, caughtByYang ? "阳星" : "阴星");
+				totalScore += 100;
+				SpawnExplosion(powers[i].x, powers[i].y, powers[i].color); // 触发粒子特效
+				switch (powers[i].type) {
+				case WALL_BREAKER:
+					wallActive = false; // 破墙
+					printf("[LOG]:隔离墙破坏\n");
+					break;
+				case SHIELD:
+					if (caughtByYang) yangHasShield = true;
+					else yinHasShield = true;
+					printf("[LOG]:%s获得盾\n", caughtByYang ? "阳星" : "阴星");
+					break;
+				case SLOW_DOWN:
+					slowDownTimer = 180; // 3秒减速
+					printf("[LOG]:减速效果触发\n");
+					break;
+				}
+				powers[i].active = false; // 道具被捕获后消失
+			}
+			// 回收逻辑
+			if (powers[i].x < 0 - powers[i].radius)
+				powers[i].active = false;
+		}
+	}
+}
+
 // 绘制障碍物
 void DrawObstacles() {
+	if(yangHasShield || yinHasShield)
+		setlinestyle(PS_DASH, 2);
 	for (int i = 0; i < MAX_OBS; i++) {
 		if (obs[i].active) {
 			setfillcolor(obs[i].color);
@@ -337,8 +474,11 @@ void DrawObstacles() {
 				(int)(obs[i].x + obs[i].w), (int)(obs[i].y + obs[i].h));
 		}
 	}
+	// 这里换实线修复中间臧爱墙也变虚线
+	setlinestyle(PS_SOLID, 2);
 }
 
+// 更新得分
 void UpdateGameLogic(struct Twin* yang, struct Twin* yin) {
 	// 基础得分
 	totalScore += 0.1f * currentDifficulty;
@@ -350,12 +490,17 @@ void UpdateGameLogic(struct Twin* yang, struct Twin* yin) {
 	// 上难度了
 	currentDifficulty = 1.0f + (float)totalScore / 500.0f;
 }
+// 面版，这个像素字体帅
 void DrawHUD() {
-    settextcolor(YELLOW);
-    settextstyle(25, 0, _T("Consolas"));
-    TCHAR scoreStr[50];
-    _stprintf(scoreStr, _T("SCORE: %.0f  LEVEL: %.1f"), totalScore, currentDifficulty);
-    outtextxy(20, 50, scoreStr);
+	settextcolor(YELLOW);
+	settextstyle(25, 0, _T("Consolas"));
+	TCHAR scoreStr[50];
+	_stprintf(scoreStr, _T("SCORE: %.0f  LEVEL: %.1f"), totalScore, currentDifficulty);
+	outtextxy(20, 50, scoreStr);
+	settextcolor(WHITE);
+	settextstyle(20, 0, _T("Arial"));
+	char tipsStr[64] = "按 ESC 键暂停/继续，按 R 键重置游戏";
+	outtextxy((getwidth() - textwidth(tipsStr) - 10), textheight(tipsStr) + 5, tipsStr);
 }
 
 int main() {
@@ -391,24 +536,25 @@ int main() {
 		BeginBatchDraw(); // 双缓冲开始，绘图开始
 
 		// 读取鼠标 and 键盘
-		while (peekmessage(&msg,EX_MOUSE | EX_KEY)) {
+		while (peekmessage(&msg, EX_MOUSE | EX_KEY)) {
 			// 按下 ESC 键切换暂停/继续
 			if (msg.message == WM_KEYDOWN && msg.vkcode == VK_ESCAPE) {
 				if (gameState == PLAYING) {
 					gameState = PAUSE; // 切换到暂停状态
 					printf("[LOG]:按下 ESC 键，游戏暂停\n");
-				} else if (gameState == PAUSE) {
+				}
+				else if (gameState == PAUSE) {
 					gameState = PLAYING; // 切换回游戏状态
 					printf("[LOG]: 按下 ESC键，回到PLAYING\n");
 				}
 				else if (gameState == GAMEOVER) {
-					printf("[LOG]:按下 ESC 键，退出游戏\n"); 
+					printf("[LOG]:按下 ESC 键，退出游戏\n");
 					exit(0);
 				}
 			}
-			if(msg.message == WM_KEYDOWN && msg.vkcode == 'R' && gameState == GAMEOVER) {
+			// 重置游戏状态
+			if (msg.message == WM_KEYDOWN && msg.vkcode == 'R' && gameState == GAMEOVER) {
 				printf("[LOG]:按下 R 键，重置游戏\n");
-				// 重置游戏状态
 				gameState = MENU;
 				printf("[LOG]:游戏状态切换到 MENU\n");
 				totalScore = 0;
@@ -417,8 +563,12 @@ int main() {
 				yang.x = 400; yang.y = 150;
 				yin.x = 400; yin.y = 450;
 				for (int i = 0; i < MAX_OBS; i++) obs[i].active = false; // 清除障碍物
-				wallActive = true;    // 间隔恢复
-				wallBreaker.active = true;
+				wallActive = true; // 间隔墙恢复
+				yangHasShield = false;  // 盾清除
+				yinHasShield = false;
+				slowDownTimer = 0; // 减速清除
+				for (int i = 0; i < MAX_PARTICLES; i++) particles[i].active = false; // 清除粒子
+				for (int i = 0; i < MAX_POWERS; i++) powers[i].active = false; // 清除道具
 			}
 
 			switch (msg.message) {
@@ -426,9 +576,9 @@ int main() {
 				curMouseX = msg.x;
 				curMouseY = msg.y;
 				break;
-			case WM_LBUTTONDOWN: 
+			case WM_LBUTTONDOWN:
 				// 菜单时
-				if(gameState == MENU) {
+				if (gameState == MENU) {
 					if (isMouseInArea(msg.x, msg.y, getwidth() / 2 - 60, getheight() / 2 - 30, 120, 40)) {
 						printf("[LOG]:点击了开始游戏按钮\n");
 						gameState = PLAYING; // 切换到游戏状态
@@ -486,45 +636,63 @@ int main() {
 				fillrectangle(0, 295, 800, 305); // 高度10
 			}
 
-			sprintf(str, "Frame: %d", totalFrames++/60);
+			sprintf(str, "Time: %d", totalFrames++ / 60);
 			outtextxy(20, 20, str);// 写入帧数
 
 			HandleInput(&yang, &yin);
 			UpdateTrails(&yang);
 			UpdateTrails(&yin);
 
-			// 障碍物逻辑
+			UpdatePowers(&yang, &yin, totalFrames);
+			UpdateAndDrawParticles();
+
 			UpdateObstacles(); // 更新障碍物坐标并随机生成
 			UpdateGameLogic(&yang, &yin); // 更新分数和难度
 			DrawHUD();
-			// 每 500 帧生成一次道具
-			if (wallActive && totalFrames % 500 == 0 && !wallBreaker.active) {
-				wallBreaker.active = true;
-				wallBreaker.x = 800;
-				wallBreaker.y = 100 + rand() % 400; // 随机位置
-				printf("[LOG]: 道具生成 TIME: %d\n", totalFrames / 60);
-			}
-			if (wallBreaker.active) {
-				wallBreaker.x -= 3.0f; // 道具向左飘
-				setfillcolor(wallBreaker.color);
-				solidcircle((int)wallBreaker.x, (int)wallBreaker.y, wallBreaker.radius);
-
-				// 判断是否吃到了道具
-				if ((abs(yang.x - wallBreaker.x) < 15 && abs(yang.y - wallBreaker.y) < 15) ||
-					(abs(yin.x - wallBreaker.x) < 15 && abs(yin.y - wallBreaker.y) < 15)) {
-					wallActive = false;    // 墙壁消失
-					printf("[LOG]:道具被吃掉，墙壁消失\n");
-					wallBreaker.active = false;
-				}
-				if (wallBreaker.x < 0) wallBreaker.active = false; // 飞出屏幕回收
-			}
 			DrawObstacles();
+
+			// 绘制盾
+			if (yangHasShield) {
+				setlinecolor(RGB(0, 255, 150));
+				// 怀疑此处可能有问题导致障碍物的虚线边框不消失，已经修复
+				setlinestyle(PS_DASH, 2);
+				circle((int)yang.x, (int)yang.y, yang.radius + 10);
+				setlinestyle(PS_SOLID, 1); //恢复实线边框
+			}
+			if (yinHasShield) {
+				setlinecolor(RGB(0, 255, 150));
+				setlinestyle(PS_DASH, 2);
+				circle((int)yin.x, (int)yin.y, yin.radius + 10);
+				setlinestyle(PS_SOLID, 1);
+			}
 
 			// 死亡判定
 			for (int i = 0; i < MAX_OBS; i++) {
-				if (CheckCollision(&yang, &obs[i]) || CheckCollision(&yin, &obs[i])) {
-					gameState = GAMEOVER;
-					printf("[LOG]: 角色死亡\n");
+				if (CheckCollision(&yang, &obs[i])) {
+					if (yangHasShield) {
+						yangHasShield = false; // 盾抵消一次碰撞
+						obs[i].active = false; // 障碍物消失
+						setlinestyle(PS_SOLID, 1); // 恢复实线边框
+						SpawnExplosion(obs[i].x, obs[i].y, obs[i].color); // 爆炸特效
+						printf("[LOG]: 阳星的盾效果触发\n");
+					}
+					else {
+						gameState = GAMEOVER;
+						printf("[LOG]: 角色死亡\n");
+					}
+				}
+				if (CheckCollision(&yin, &obs[i])) {
+					if (yinHasShield) {
+						yinHasShield = false; // 盾抵消一次碰撞
+						obs[i].active = false; // 障碍物消失
+						setlinestyle(PS_SOLID, 1); // 恢复实线边框
+						SpawnExplosion(obs[i].x, obs[i].y, obs[i].color);
+						printf("[LOG]: 阴星的盾效果触发\n");
+					}
+					else {
+						gameState = GAMEOVER;
+						printf("[LOG]: 角色死亡\n");
+					}
 				}
 			}
 
@@ -532,25 +700,35 @@ int main() {
 			DrawYangStar(&yang);
 			DrawTrails(&yin, COLOR_YIN_CORE);
 			DrawYinStar(&yin);
-		// 游戏结束界面
-		} else if (gameState == GAMEOVER) {
-			char finalScore[50];
-			sprintf(finalScore, "最终得分：%.0f", totalScore);
+			// 游戏结束界面
+		}
+		else if (gameState == GAMEOVER) {
+			char tips[50];
 			settextcolor(WHITE);
 			settextstyle(40, 0, _T("Arial"));
-			outtextxy(getwidth() / 2 - 100, getheight() / 2 - 100, _T("游戏结束！"));
-			outtextxy(getwidth() / 2 - 100, getheight() / 2 - 50, finalScore);
-			outtextxy(getwidth() / 2 - 150, getheight() / 2 + 20, _T("按 R 键重新开始"));
+			outtextxy(getwidth() / 2 - 100, getheight() / 2 - 150, _T("游戏结束！"));
+			sprintf(tips, "最终得分：%.0f", totalScore);
+			outtextxy((getwidth() - textwidth(tips)) / 2, getheight() / 2 - 100, tips);
+			if (totalFrames /60 < 10)
+				sprintf(tips,"总共坚持了 %d S,不会是一个人玩的吧？", totalFrames / 60);
+			else if (totalFrames / 60 < 30)
+				sprintf(tips,"总共坚持了 %d S, 应该看到了全部彩蛋？", totalFrames / 60);
+			else if (totalFrames / 60 < 60)
+				sprintf(tips,"总共坚持了 %d S, 是个游戏糕手", totalFrames / 60);
+			else
+				sprintf(tips, "{\"comment\":\"开了\",\"Time\":%d s}", totalFrames / 60);
+			outtextxy((getwidth() - textwidth(tips)) / 2, getheight() / 2 - 50, tips);
+			outtextxy(getwidth() / 2 - 150, getheight() / 2 + 15, _T("按 R 键重新开始"));
 		}
-			
+
 		EndBatchDraw(); // 双缓冲结束，画面绘制结束
 
 		// 帧率控制
 		frametime = clock() - startTime;
-		if(totalFrames % 60==0 && totalFrames)
-			printf("[LOG]:帧 %d 耗时 %ld ms time(s):%d\n", totalFrames, frametime, totalFrames /60);
+		if (totalFrames % 60 == 0 && totalFrames)
+			printf("[LOG]:帧 %d 耗时 %ld ms time(s):%d\n", totalFrames, frametime, totalFrames / 60);
 		if (frametime < FPS) {
-			Sleep(FPS-frametime); // 60 FPS
+			Sleep(FPS - frametime); // 60 FPS
 		}
 	}
 	closegraph();
