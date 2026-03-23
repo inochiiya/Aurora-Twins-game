f49034909c32d5f7095d24c483bc795339f36b58
Inochi <sunyifu2022@outlook.com>
2026-03-23 23:44:04 +0800

    修改仓库名

--- c++miniGame/Two Ball Run .cpp
+++ c++miniGame/Two Ball Run .cpp
@@ -157,7 +157,7 @@ void ShowInochiLogo() {
 
 	printf("    >> TBR GAME PROJECT - Developer Mode Active\n");
 	printf("    >> Welcome back, Inochi. Current UID: 1145\n"); // 
-	printf("    >> GitHub: https://github.com/inochiiya/Aurora-Twins-game/\n");
+	printf("    >> GitHub: https://github.com/inochiiya/Two-Ball-Run\n");
 	printf("    ==================================================\n\n");
 }
 
